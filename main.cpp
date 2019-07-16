99cb061a7eebd51dfe25aa4cc6a0652883129c00
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2019-07-16 09:54:34 +0100

    init LL and run the wsf dispatcher

--- main.cpp
+++ main.cpp
@@ -19,6 +19,14 @@
 #include "ControllerToHost.h"
 #include "util/CordioHCIHook.h"
 #include "util/HostSerial.h"
+#include "wsf_types.h"
+#include "wsf_buf.h"
+#include "wsf_msg.h"
+#include "wsf_os.h"
+#include "wsf_buf.h"
+#include "wsf_timer.h"
+#include "hci_handler.h"
+#include "mbed.h"
 
 using ble::vendor::cordio::CordioHCIHook;
 
@@ -26,13 +34,56 @@ using ble::vendor::cordio::CordioHCIHook;
 HostToController host_to_controller(util::get_host_serial());
 ControllerToHost controller_to_host(util::get_host_serial());
 
+extern uint8_t *SystemHeapStart;
+extern uint32_t SystemHeapSize;
+
+void init_wsf(ble::vendor::cordio::buf_pool_desc_t& buf_pool_desc) {
+    // use the buffer for the WSF heap
+    SystemHeapStart = buf_pool_desc.buffer_memory;
+    SystemHeapSize = buf_pool_desc.buffer_size;
+
+    // Initialize buffers with the ones provided by the HCI driver
+    uint16_t bytes_used = WsfBufInit(
+        buf_pool_desc.pool_count,
+        (wsfBufPoolDesc_t*)buf_pool_desc.pool_description
+    );
+
+    // Raise assert if not enough memory was allocated
+    MBED_ASSERT(bytes_used != 0);
+
+    SystemHeapStart += bytes_used;
+    SystemHeapSize -= bytes_used;
+
+    WsfTimerInit();
+}
+
 int main() {
-    CordioHCIHook::get_driver().initialize();
+    ble::vendor::cordio::CordioHCIDriver& hci_driver = CordioHCIHook::get_driver();
+    ble::vendor::cordio::buf_pool_desc_t buf_pool_desc = hci_driver.get_buffer_pool_description();
+
+    init_wsf(buf_pool_desc);
+
+    hci_driver.initialize();
+
     host_to_controller.start();
     controller_to_host.start();
 
+    uint64_t last_update_us;
+    mbed::LowPowerTimer timer;
+
     while (true) {
-        rtos::Thread::wait(osWaitForever);
+        last_update_us += (uint64_t) timer.read_high_resolution_us();
+        timer.reset();
+
+        uint64_t last_update_ms = (last_update_us / 1000);
+        wsfTimerTicks_t wsf_ticks = (last_update_ms / WSF_MS_PER_TICK);
+
+        if (wsf_ticks > 0) {
+            WsfTimerUpdate(wsf_ticks);
+            last_update_us -= (last_update_ms * 1000);
+        }
+
+        wsfOsDispatcher();
     }
 
     return 0;
